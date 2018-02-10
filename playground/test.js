cebbf1cd5bce4cd659288b554602d1a3e5e5b742
Kimmy <kimmy@MacBook-Pro.local>
2018-02-10 01:55:32 -0800

    new project setup

---
+++ playground/test.js
@@ -0,0 +1,3 @@
+var string = 'this is a string';
+
+console.log(`Insert a string here: ${string}`); //ECMAScript 6 feature using ``
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname,'../public');//get a corret path address
+const port = process.env.PORT || 3000 ;
+var app = express();
+
+
+app.use(express.static(publicPath));
+app.listen(port, () => {
+   console.log(`server is up on ${port}`); // using `` . that was ECMAScript 6 feature ,template literals
+});
