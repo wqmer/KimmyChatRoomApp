1dd773bba28948b4fc8de5138103fd5fe6ba38ee
Kimmy <kimmy@MacBook-Pro.local>
2018-02-10 18:25:55 -0800

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname,'../public/view');//get a corret path address
-const port = process.env.PORT || 3000 ;
-var app = express();
+const port = process.env.PORT || 3000 ;//configure port pram on heroku
+var app = express();//caeate express mdiileware
+var server = http.createServer(app);//create server
+var io = socketIO(server); //create socket communicate with client on server .
 
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
-   console.log(`server is up on ${port}`); // using `` . that was ECMAScript 6 feature ,template literals
+
+io.on('connection',(socket) => {
+     console.log('New user connected');
+     socket.on('disconnect', () => {
+     console.log('User disconnected');
+   });     
+});
+
+
+
+
+server.listen(port, () => {
+   console.log(`server is up on ${port}`); // using `` template literals by ECMAScript 6 feature
 });
